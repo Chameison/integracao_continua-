dc0a450f05e9e9c9be28c4d94509e6af31f7dcc8
Chameison Araújo <chameison.acre@gmail.com>
2022-11-19 21:11:47 -0300

    Configurando cors e env

--- back-end/src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
+++ back-end/src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
@@ -18,8 +18,8 @@ public class FiltroCors {
         corsConfig.setAllowCredentials(true);
         corsConfig.setAllowedOrigins(
             Arrays.asList(
-                "http://127.0.0.1:5500",
-                "https://localhost:4200")
+                "https://sgcmchdev.up.railway.app/",
+                "https://sgcmchdev.up.railway.app/")
         );
         corsConfig.setAllowedMethods(Arrays.asList("*"));
         corsConfig.setAllowedHeaders(Arrays.asList("*"));
