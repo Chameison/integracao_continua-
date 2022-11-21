09aec1205576ee887c8cd88ba58edfc9a6af3254
Chameison Araújo <chameison.acre@gmail.com>
2022-11-21 15:11:25 -0300

    erro

--- back-end/src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
+++ back-end/src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
@@ -18,7 +18,8 @@ public class FiltroCors {
         corsConfig.setAllowCredentials(true);
         corsConfig.setAllowedOrigins(
             Arrays.asList(
-                "https://sgcmchdev.up.railway.app/")
+                "https://sgcmchdev.up.railway.app/",
+                "https://sgcmchdev2-production.up.railway.app/")
         );
         corsConfig.setAllowedMethods(Arrays.asList("*"));
         corsConfig.setAllowedHeaders(Arrays.asList("*"));
