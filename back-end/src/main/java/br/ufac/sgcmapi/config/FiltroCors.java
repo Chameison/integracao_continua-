20cbae1d61e9ce28eb3ae8a3e8ee43c031d9dca8
Chameison <chameison.acre@gmail.com>
2022-11-21 19:18:20 -0500

    removendo a barra do env

--- back-end/src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
+++ back-end/src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
@@ -18,8 +18,8 @@ public class FiltroCors {
         corsConfig.setAllowCredentials(true);
         corsConfig.setAllowedOrigins(
             Arrays.asList(
-                "https://sgcmchdev.up.railway.app/",
-                "https://sgcmchdev2-production-b512.up.railway.app/")
+                
+                "https://sgcmchdev2-production-b512.up.railway.app")
         );
         corsConfig.setAllowedMethods(Arrays.asList("*"));
         corsConfig.setAllowedHeaders(Arrays.asList("*"));
